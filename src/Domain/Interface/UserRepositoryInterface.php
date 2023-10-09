5f665574a17358cb48eacc43b6da0b29440fabe6
Jakub Skowron <jakub@skowron.dev>
2023-10-09 10:09:55 +0200

    User repository

---
+++ src/Domain/Interface/UserRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Domain\Interface;
+
+use App\Domain\Models\User;
+
+interface UserRepositoryInterface
+{
+    public function find(int $id): ?User;
+    public function findAll(): array;
+    public function save(User $user): void;
+    public function delete(User $user): void;
+}
