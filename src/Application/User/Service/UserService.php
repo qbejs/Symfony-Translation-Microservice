e2ce6d130a11f404db3481327019a495af3d47a1
Jakub Skowron <jakub@skowron.dev>
2023-10-10 17:10:43 +0200

    User service and create user command

---
+++ src/Application/User/Service/UserService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Application\User\Service;
+
+use App\Application\Interface\ServiceInterface;
+use App\Domain\Factory\UserFactory;
+use App\Domain\Interface\UserRepositoryInterface;
+use App\Domain\Models\User;
+use App\Domain\Models\ValueObject\User\Password;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserService implements ServiceInterface
+{
+    public function __construct(
+        private readonly UserRepositoryInterface $userRepository,
+        private readonly UserPasswordHasherInterface $userPasswordHasher
+    )
+    {
+    }
+    public function getResultFromMessage(Envelope $message): mixed
+    {
+        return $message->last(HandledStamp::class)->getResult();
+    }
+
+    public function createUser(string $username, string $email, string $password): ?User
+    {
+        try {
+            $userFactory = new UserFactory();
+            $user = $userFactory->create($username, $email, null);
+            $user->setPassword(new Password($this->userPasswordHasher->hashPassword($user, $password)));
+            $this->userRepository->save($user);
+
+            return $user;
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+}
